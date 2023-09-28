c1cdde91327e676835cb189c833eeef7b62f9db9
BABEL-SI\miguel.ggarrido <miguel.ggarrido@babelgroup.com>
2023-09-28 09:42:21 +0200

    cambiando props

--- static_components/src/components/Card.jsx
+++ static_components/src/components/Card.jsx
@@ -1,8 +1,8 @@
-const Card = (props) => {
+const Card = ({ img, description }) => {
   return (
     <div className='flex flex-col border-2 border-teal-600 rounded p-3'>
-      <img src={props.img} />
-      <p>{props.description}</p>
+      <img src={img} />
+      <p>{description}</p>
     </div>
   )
 }
