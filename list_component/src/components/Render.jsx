ebd18bd4749952b6c090d56613ec6993b48722eb
BABEL-SI\miguel.ggarrido <miguel.ggarrido@babelgroup.com>
2023-09-28 08:59:01 +0200

    subiendo ejercicio

--- list_component/src/components/Render.jsx
+++ list_component/src/components/Render.jsx
@@ -1,21 +1,23 @@
 const Render = ({ data }) => {
   const handleClick = (item) => {
-    alert(`Datos del elementos seleccionado: \nNombre: ${item.name}\nColor: ${item.color}`)
+    window.alert(`Datos del elementos seleccionado: \nNombre: ${item.name}\nColor: ${item.color}`)
   }
   return (
     <div>
       <ul>
-        {data.map((item, index) => (
-          <li key={index}>
+        {data.map((item) => (
+          <li key={item.name}>
             <span style={{ color: item.color, fontWeight: item.checked ? 'bold' : 'normal' }}>
               {item.name}
             </span>
-            <button onClick={() => handleClick(item)}>Mostrar Alerta</button>
+            <button onClick={(e) => handleClick(item)}>Mostrar Alerta</button>
           </li>
         ))}
       </ul>
     </div>
   )
 }
+
+// no coger index para el key
 
 export default Render
