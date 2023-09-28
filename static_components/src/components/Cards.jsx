687ca3c34131cfc496fbf3f5f18ac61c139a1a21
BABEL-SI\miguel.ggarrido <miguel.ggarrido@babelgroup.com>
2023-09-28 09:44:47 +0200

    last commit

--- static_components/src/components/Cards.jsx
+++ static_components/src/components/Cards.jsx
@@ -2,14 +2,17 @@ import Card from './Card'
 
 const cards = [
   {
+    id: 0,
     image: 'https://picsum.photos/id/9/400/250',
     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit error, eos recusandae nihil ipsum distinctio natus accusamus repellat est in fugiat vitae quia quasi architecto hic voluptatibus nulla dolor elit'
   },
   {
+    id: 1,
     image: 'https://picsum.photos/id/0/400/250',
     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit error, eos recusandae nihil ipsum distinctio natus accusamus repellat est in fugiat vitae quia quasi architecto hic voluptatibus nulla dolor elit'
   },
   {
+    id: 2,
     image: 'https://picsum.photos/id/8/400/250',
     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit error, eos recusandae nihil ipsum distinctio natus accusamus repellat est in fugiat vitae quia quasi architecto hic voluptatibus nulla dolor elit'
   }
@@ -18,8 +21,8 @@ const cards = [
 const Cards = () => {
   return (
     <div className='flex gap-10 m-5 text-justify'>
-      {cards.map((element, index) => (
-        <Card key={index} img={element.image} description={element.description} />
+      {cards.map((element, id) => (
+        <Card key={id} img={element.image} description={element.description} />
       ))}
     </div>
   )
