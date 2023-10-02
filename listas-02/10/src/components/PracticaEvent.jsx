05dd209dc67062bb0885ed19188681d9d801fb57
BABEL-SI\miguel.ggarrido <miguel.ggarrido@babelgroup.com>
2023-10-02 14:56:52 +0200

    02/10/23

--- listas-02/10/src/components/PracticaEvent.jsx
+++ listas-02/10/src/components/PracticaEvent.jsx
@@ -41,3 +41,47 @@ export default PracticaEvent
 
 /* Crear un componente y capturar si tiene el ratón por encima y si tiene o no tiene el foco. Aplicaremos estilos en función de cada uno de los posibles estados del elemento.
 Huelga decir que esto es realizable mediante CSS con los selectores :hover y :focus pero lo vamos a hacer de forma programática para tener el control total del comportamiento del componente. */
+
+/*
+
+function ContenedorEventos() {
+
+  const [isHover, setIsHover] = useState(false)
+  const [isFocus, setIsFocus] = useState(false)
+
+  function handleHover(inside) {
+    console.log('Hover', inside)
+    setIsHover(inside)
+  }
+
+  function handleFocus(hasFocus) {
+    console.log('Focus', hasFocus)
+    setIsFocus(hasFocus)
+  }
+
+  function getClassName() {
+    const classList = []
+    classList.push(isHover ? 'hover' : 'no-hover')
+    classList.push(isFocus ? 'focus' : 'no-focus')
+    return classList.join(' ')
+  }
+
+  return (
+    <div
+      autoFocus
+      tabIndex={0}
+      className={getClassName()}
+      onFocus={() => handleFocus(true)}
+      onBlur={() => handleFocus(false)}
+      onMouseEnter={() => handleHover(true)}
+      onMouseLeave={() => handleHover(false)}
+    >
+      <p>Lorem ipsum dolor setTimeout</p>
+      <p>Lorem ipsum dolor setTimeout</p>
+      <p>Lorem ipsum dolor setTimeout</p>
+    </div>
+  )
+
+}
+
+*/
